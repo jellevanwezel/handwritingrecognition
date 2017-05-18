49dc50e897ab527bd060433d30491632c2418c6c
Jelle van Wezel <jelle.van.wezel@gmail.com>
2017-05-18 12:36:07 +0200

    .

--- implementation/segmentation/segmentation.m
+++ implementation/segmentation/segmentation.m
@@ -1,13 +1,13 @@
 % Segmentation Demo
 
-%A = imread('../../dataset/Train/navis-Ming-Qing_18341_0004-line-001-y1=0-y2=289.pgm');
-A = imread('../../dataset/Train/navis-Ming-Qing_18637_0022-line-009-y1=1226-y2=1386.pgm');
+A = imread('../../dataset/Train/navis-Ming-Qing_18341_0004-line-001-y1=0-y2=289.pgm');
+%A = imread('../../dataset/Train/navis-Ming-Qing_18637_0022-line-009-y1=1226-y2=1386.pgm');
 binA = seg_binairy(A);
 
-[rotatedA,angle] = seg_rotation(binA);
+%[rotatedA,angle] = seg_rotation(binA);
 % angle for this image = 0.9
 % calculating the angle takes a long time :/
-%rotatedA = imcomplement(imrotate(imcomplement(binA),0.9));
+rotatedA = imcomplement(imrotate(imcomplement(binA),0.9));
 
 vCroppedA = seg_v_density(rotatedA,0);
 
