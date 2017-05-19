516b23b6f183b79011269d2f4127e2ba74c45872
Jelle van Wezel <jelle.van.wezel@gmail.com>
2017-05-19 15:08:45 +0200

    rotation

--- implementation/segmentation/seg_rotation.m
+++ implementation/segmentation/seg_rotation.m
@@ -8,7 +8,7 @@ A = imcomplement(A);
 maxDensity = -inf;
 optimalAngle = 0;
 
-for angle = -2:0.1:2
+for angle = -1:0.1:1
     rotImage = imrotate(A,angle);
     rotImage = imresize(rotImage,[floor(size(rotImage,1)/3), size(rotImage,2)]);
     if(max(sum(rotImage,2)) > maxDensity)
