1ebc11089fe8df12ef933e75cdeec9ef158d1d96
Jelle van Wezel <jelle.van.wezel@gmail.com>
2017-05-23 15:06:31 +0200

    merge groups fixed

--- implementation/segmentation/script_segmentation_demo.m
+++ implementation/segmentation/script_segmentation_demo.m
@@ -1,14 +1,14 @@
 % Segmentation Demo
 
-A = imread('../../dataset/Train/navis-Ming-Qing_18341_0004-line-001-y1=0-y2=289.pgm');
-%A = imread('../../dataset/Train/navis-Ming-Qing_18637_0022-line-009-y1=1226-y2=1386.pgm');
+%A = imread('../../dataset/Train/navis-Ming-Qing_18341_0004-line-001-y1=0-y2=289.pgm');
+A = imread('../../dataset/Train/navis-Ming-Qing_18637_0022-line-009-y1=1226-y2=1386.pgm');
 binA = seg_binairy(A);
 
-%[rotatedA,angle] = seg_rotation(binA);
+[rotatedA,angle] = seg_rotation(binA);
 
 % angle for the first image = 0.9
 % calculating the angle takes a long time :/
-rotatedA = imcomplement(imrotate(imcomplement(binA),0.9));
+%rotatedA = imcomplement(imrotate(imcomplement(binA),0.9));
 
 vCroppedA = seg_v_density(rotatedA,0);
 
@@ -16,6 +16,6 @@ trimmedA = seg_trim_image(vCroppedA);
 
 merged = seg_concomp(trimmedA);
 
-[big,small] = seg_get_outliers(merged)
+[big,small] = seg_get_outliers(merged);
 
 seg_show_chars(A,merged,6); %6 is the amount of columns
--- implementation/segmentation/seg_merge_groups.m
+++ implementation/segmentation/seg_merge_groups.m
@@ -1,6 +1,6 @@
-function [ framedComps ] = seg_merge_groups( components, A)
+function [ framedComps, components ] = seg_merge_groups( components, A)
 
-overlap_threshold = 0.15;
+overlap_threshold = 0.4;
 %the percentage of overlap between components for them to be merged
 
 imageDims = size(A);
@@ -27,10 +27,16 @@ for i = 1:length(components)
             %These components are not overlapping
             continue;
         end
-        overlap = abs((abs(minCol - minCol2) + abs(maxCol - maxCol2)) - width1);
-        if 1 - (overlap / width2) >= overlap_threshold
+        nonOverlap = abs(minCol - minCol2) + abs(maxCol - maxCol2);
+        totalLength = abs(min([minCol,minCol2]) - max([maxCol, maxCol2]));
+        overlap = totalLength - nonOverlap;
+        overlapPercentage = overlap / width2;
+        if overlapPercentage >= overlap_threshold
             components{i} = [components{i};components{j}];
             components{j} = [];
+            width1 = totalLength;
+            minCol = min([minCol,minCol2]);
+            maxCol = max([maxCol, maxCol2]);
         end
         
     end
@@ -38,6 +44,5 @@ for i = 1:length(components)
 end
 components = components(~cellfun(@isempty, components));
 framedComps = seg_canvas_comps( components , A);
-
 end
 
