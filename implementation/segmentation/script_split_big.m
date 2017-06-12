e7c416c7b5636cbed34a889602d72d385171dcf7
Jelle van Wezel <jelle.van.wezel@gmail.com>
2017-06-12 17:29:14 +0200

    big split

---
+++ implementation/segmentation/script_split_big.m
@@ -0,0 +1,47 @@
+% Before running, make sure your dataset is in ../../dataset/Train/
+% Also make sure you have /implementation/segmentation as your working
+% directory
+
+clear;
+dataDir = '../../dataset/Train_segmented/';
+outputDir = strcat(dataDir,'../Train_split_big/');
+mkdir(outputDir);
+dirContents = dir(dataDir);
+
+meanWidth = 73.5944; % Mean width calculated from the labeled data
+stdWidth = 8.4463; % std in width calculated from the labeled data
+
+for i = 3:10 %size(dirContents,1)    
+    if ~dirContents(i).isdir
+        continue;
+    end
+    
+    folderName = dirContents(i).name;
+    folderDir = dir([dataDir,folderName(1:end-4),'/']);
+    
+    for j = 3:size(folderDir,1)
+        fileName = folderDir(j).name;
+        if ~all(fileName(end-3:end) == '.png')
+            continue;
+        end 
+                
+        A = imread(strcat(folderName,fileName));
+        if size(A,2) > meanWidth + 3 * stdWidth
+            continue;
+        end
+        
+        splitted = seg_split_big(A,meanWidth,stdWidth,10);
+        
+        if size(splitted,2) == 1
+           %image not splitted
+           continue;
+        end
+        outputPath([outputDir,folderName,'/',num2str(j),'/']);
+        mkdir(outputPath); 
+        
+        for k = 1:length(splitted)
+            imageSplit =  splitted{k};
+            imwrite(I,strcat(outputPath,'split_',num2str(k),'.png'));
+        end
+    end
+end
--- implementation/segmentation/seg_split_big.m
+++ implementation/segmentation/seg_split_big.m
@@ -1,12 +1,72 @@
-function [ ] = seg_split_big( )
-    % look at the big char, also look at the chars next to this char
+function [ splitted ] = seg_split_big(I, meanWidth, stdWidth, avgFilter)
+     
     
-    % look at the white space between the chars, try to minimize white
-    % space in the chars, white space means cut
+    %Search for pixelgroups
 
-    % find where the centers of minima are
-    % find the the closest minima to the predicted amount of image cuts
-    % cut there :D
-
+    diffColls = zeros(1,size(I,2));
+    for i = 1:size(I,2);
+        colDiff = diff(I(:,i));
+        colDiff(colDiff<1) = 0;
+        diffColls(i) = sum(colDiff);
+    end
+    binDiffs = filter(1/avgFilter * ones(1,avgFilter),1, diffColls);
+    binDiffs(binDiffs <= 1) = 0;
+    binDiffs(binDiffs > 1) = 1;
+    startCol = floor(meanWidth - 3 * stdWidth);
+    endCol =  size(I,2) - floor(meanWidth - 1 * stdWidth);
+    
+    %Check if there is atleast one group if not return the old image
+    if sum(binDiffs(startCol:endCol)) == 0
+        splitted = cell(1);
+        splitted{1} = I;
+        return;
+    end
+    
+    %Find pixelgroups, locations and width
+    whiteSpaceRegions = [];
+    inGroup = 0;
+    for i = startCol:endCol
+        
+        if binDiffs(i) == 1 && inGroup == 0
+            inGroup = 1;
+            whiteSpaceRegions(end+1,:) = [i,endCol, abs(i-endCol)]; 
+        end
+        
+        if binDiffs(i) == 0 && inGroup == 1
+            inGroup = 0;
+            whiteSpaceRegions(end,2) = i;
+            whiteSpaceRegions(end,3) = abs(whiteSpaceRegions(end,1) - i);
+        end
+    end
+    
+    %Get biggest whitespace location
+    sWSR = sortrows(whiteSpaceRegions,3);
+    splitLoc = sWSR(1,1) + round(0.5 * sWSR(1,3)) + avgFilter;
+    
+    %Split the image and return the 2 splits.
+    A = I(:,1:splitLoc);
+    B = I(:,splitLoc:end);
+    
+    splitted = cell(0);
+        
+    if size(A,2) > meanWidth + 3 * stdWidth
+        splittedR = seg_split_big(A,meanWidth,stdWidth,10);
+        if length(splittedR) > 1
+            splitted = {splitted{:},splittedR{:}};
+        end
+    else
+        A = seg_trim_image(A);
+        splitted = {splitted{:},A};
+    end
+    
+    if size(B,2) > meanWidth + 3 * stdWidth
+        splittedR = seg_split_big(B,meanWidth,stdWidth,10);
+        if length(splittedR) > 1
+            splitted = {splitted{:},splittedR{:}};
+        end
+    else
+        B = seg_trim_image(B);
+        splitted = {splitted{:},B};
+    end
 end
 
